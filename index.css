759b826bda53fc1c66b465bcda3afca21e3d08d2
comumendez <130485328+comumendez@users.noreply.github.com>
2023-04-25 20:57:32 -0600

    Add files via upload

---
+++ index.css
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mi página Web</title>
+
+ <style type="text/css">
+        body {
+            background-color: lavender;
+            font-family: serif;
+        }
+        
+        h1 {
+            color: darkblue;
+            font-size: 32px;
+            font-style: italic;
+            text-align: center;
+        }
+
+        h2 {
+            color: darkblue;
+            font-size: 28px;
+            font-style: italic;
+        }
+
+        h3 {
+            color: darkblue;
+            font-size: 24px;
+            font-style: italic;
+        }
+
+        p {
+            text-align: justify-all;
+            font-size: 24px;
+        }
+
+        .principal {
+            background-color: white;
+            padding: 50px;
+            margin: 25px;
+        }
+
+        .codes {
+            padding: 10px;
+            margin: 10px;
+            background-color: grey;
+            font-size: 20px;
+            color: darkred;
+            text-align: center;
+        }
+
+        img {
+            height: 500px;
+            width: auto;
+        }
+
+    </style>
+
+</head>
+<body>
+<h1>Historia de la alta costura por Constanza Munguía</h1>
+<p>
+La alta costura surgió en el siglo XVIII con Rose Bertin, la diseñadora de modas francesa de la reina María Antonieta, considerada una de las primeras en introducir la moda y la alta costura en la cultura francesa.El liderazgo francés en la moda europea se consolidó a finales del xviii, cuando la influencia, que venía del arte, la arquitectura, la música, y la moda de la corte francesa en Versalles fue imitada en toda Europa. Los que visitaban París regresaban con prendas rápidamente imitadas por costureros locales. Mujeres con estilo también ordenaban muñecas vestidas con la última moda parisina para usarlas como modelos. <br \>
+</p>
+
+
+    <img src="foto1.jpg" width="200px" height="150px">
+
+<p>
+Las colecciones de alta costura modernizada no se diseñan para venderse, <em>sino para las pasarelas.</em> En lugar de ser creadas con el propósito de venderse y generar ganancias, éstas son hechas para ampliar la publicidad, al igual que para entender y mejorar la percepción de la imagen de la marca.
+</p>
+
+<h2>Actualidad </h2>
+<p>
+En la Francia moderna <b>«alta costura» </b> es un nombre protegido que solo pueden utilizar empresas que cumplan ciertos estándares definidos. Sin embargo, ese mismo término sirve, en sentido amplio, para denominar las prendas de moda hechas a la medida, ya sea que se hayan producido en París o en otras capitales de la moda, como Londres, Milán, Nueva York o Tokio.
+<p>
+
+Entonces, la <b>alta costura</b> puede referirse a: las casas de moda o diseñadores de moda que crean modas exclusivas y que, a menudo, imponen tendencias o a las piezas de indumentaria realizadas de forma exquisitamente artesanal.
+</p>
+
+ <img src="foto2.jpg" width="200px" height="150px">
+
+<h3>Haute Couture</h3>
+<p>
+En Francia, el término Haute Couture está protegido por la ley y definido por la <i>Chambre de commerce et d'industrie de Paris</i> (Cámara de Comercio y de Industria de París) con sede en París, Francia. <i>La Chambre Syndicale de la Haute Couture</i> (Cámara Sindical de Alta costura) está definida como <b>«comisión reguladora que determina qué casas de moda son elegibles para ser verdaderas casas de Alta costura»</b>. Sus reglas declaran que solo «esas compañías mencionadas en la lista hecha cada año por una comisión situada en el Ministerio de Industria tienen derecho a usar la designación "Haute Couture"
+</p>
+
+<h4
+
+<h3>Para más información, te dejo la página de Vogue en donde explican más sobre la historia de la alta costura en la moda, y por qué sigue siendo tan importante:</h3>
+<a href="https://www.vogue.es/moda/articulos/alta-costura-otono-2022-desfiles-importancia" target="_blank"_>Vogue CLICK AQUÍ</a>"
+
+
+
+
+</body>
+</html>
